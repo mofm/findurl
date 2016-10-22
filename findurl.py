c04f73333b0033e1131c4183f34477ef02130b19
mofm <emre.eryilmaz@piesso.com>
2016-10-22 23:27:42 +0300

    add sys.arg[]

--- findurl.py
+++ findurl.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import re
 
-with open('test') as f:
+with open(sys.argv[1], 'r') as f:
     rapor = open("rapor.txt", "w")
     for line in f.readlines():
         match = re.search("(?P<url>https?://[^\s]+)", line)
         if match is not None:
             i = match.group("url")
-            rapor.write(i+"\n")
+            rapor.write(i + "\n")
 
 rapor.close()
